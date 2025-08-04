52dbced21c99b13f608f36dd707c6be95d1bd997
Johnny Le <johnnyle1410@gmail.com>
2025-08-04 14:28:09 +0200

    Learning argparse

--- core/cli.py
+++ core/cli.py
@@ -1,9 +1,22 @@
-import argparse
+from argparse import ArgumentParser
+from datetime import datetime
+from tabulate import tabulate
 
-parser = argparse.ArgumentParser(description="A simple CLI tool")
-parser.add_argument("name", help="Your name")
-parser.add_argument("--greet", help="Custom greeting", default="Hello")
+import json
+import os
+import sys
 
-args = parser.parse_args()
+## TODO
 
-print(f"{args.greet}, {args.name}!")
+def main() -> None:
+    parser = ArgumentParser(description="CLI tool")
+    # parser.add_subparsers
+    parser.add_argument("name", help="Your name")
+    parser.add_argument("--greet", help="Custom greeting", default="Hello")
+
+    args = parser.parse_args()
+
+    print(f"{args.greet}, {args.name}")
+
+if __name__ == "__main__":
+    main()
