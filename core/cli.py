de9dcb87ed21a544f29d74cf2f45054e50b05ff4
Johnny Le <johnnyle1410@gmail.com>
2025-08-04 14:40:05 +0200

    Layout

--- core/cli.py
+++ core/cli.py
@@ -7,6 +7,7 @@ import os
 import sys
 
 ## TODO
+## Task properties: | id | description | status | createdAt | updatedAt |
 
 def main() -> None:
     parser = ArgumentParser(description="CLI tool")
@@ -17,6 +18,29 @@ def main() -> None:
     args = parser.parse_args()
 
     print(f"{args.greet}, {args.name}")
+
+def load_database():
+    pass
+
+def save_database():
+    pass
+
+def add_task():
+    pass
+
+def delete_task():
+    pass
+
+def update_task():
+    pass
+
+## list all tasks
+## list by all tasks by progression
+def list_tasks():
+    pass
+
+def change_status():
+    pass
 
 if __name__ == "__main__":
     main()
